85b59dde73c10b24d3ce734d992ef533b6180c14
wanghai673 <1154103986@qq.com>
2025-03-17 22:25:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# function_calling
+ 手写function calling，使用glm-4-plus
---
+++ test.py
@@ -0,0 +1,3 @@
+from argostranslate import translate
+translated_text = translate("Hello, world!", "en", "zh")
+print(translated_text)
