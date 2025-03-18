0ba68d46e433a013622f674b082835a488d697df
wanghai673 <1154103986@qq.com>
2025-03-18 11:10:27 +0800

    翻译使用prompt操作
    翻译使用prompt操作

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from zhipuai import ZhipuAI
 import requests
 import json
-from googletrans import Translator
 
-ZHIPU_API_KEY = "da0c31cb5c9041a2bf2afaa95fa0d520.55m7NFjHmfZWV3d0"
-WEATHER_API_KEY = "8bc3a9fcfcee76dd299be4a975cfe9c6"
+
 
 # 查询天气函数
 def fetch_real_weather(location: str, unit: str = "celsius"):
-    location = translator.translate(location, dest='en').text
-
     """调用真实的天气 API 获取天气信息"""
     url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={WEATHER_API_KEY}&units={'metric' if unit == 'celsius' else 'imperial'}"
     response = requests.get(url)
@@ -36,9 +32,12 @@ def parse_res(res):
         messages.append(message)
 
 # 初始配置
-translator = Translator()
+
 client = ZhipuAI(api_key=ZHIPU_API_KEY)
+system_prompt = "你是一个天气查询小助手，请注意调用函数get_weather时，传入的参数需要翻译为英文。"
 messages = []
+messages.append({"role": "system", "content": system_prompt})
+
 tools = [
     {
         "type": "function",
--- test.py
+++
@@ -1,3 +0,0 @@
-from argostranslate import translate
-translated_text = translate("Hello, world!", "en", "zh")
-print(translated_text)
